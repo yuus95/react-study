eb4c1d14ed93f8384188ba345cb5012f4efa8cfc
yushin <kkad45@naver.com>
2023-12-01 00:06:27 +0900

    feat/react SearchForm 제어컴포넌트로 수정

--- virtual-dom/component/src/App.js
+++ virtual-dom/component/src/App.js
@@ -3,21 +3,38 @@ import Header from "./components/Header.js";
 import SearchForm from "./components/SearchForm"
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            searchKeyword: "",
+        }
+    }
 
     search(searchKeyword) {
-        console.log("TODO : searchKeyword" + searchKeyword)
+        console.log("TODO : searchKeyword " + searchKeyword)
     }
 
     reset() {
         console.log("TODO: reset start")
     }
 
+    handleChangeInput(searchKeyword) {
+        if (searchKeyword.length <= 0) {
+            this.reset();
+        }
+
+        this.setState({searchKeyword});
+    }
+
     render() {
         return (
             <>
                 <Header title="검색"/>
-                <SearchForm onSubmit={(searchKeyword) => this.search(searchKeyword)}
-                            onReset={() => this.reset()}/>
+                <SearchForm value={this.state.searchKeyword}
+                            onSubmit={(searchKeyword) => this.search(searchKeyword)}
+                            onReset={() => this.reset()}
+                            onChange={(value) => this.handleChangeInput(value)}
+                />
             </>
         )
             ;
--- virtual-dom/component/src/components/SearchForm.js
+++ virtual-dom/component/src/components/SearchForm.js
@@ -1,59 +1,34 @@
-import React from "react";
+import React from 'react'
 
+const SearchForm = ({value, onSubmit, onReset, onChange}) => {
 
-export default class SearchForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchKeyword: "",
-        }
-    }
-
-    //폼에 입력이벤트가 발생하면 서버에 데이터를 요청하는데 이벤트를 막는다.
-    //props 외부에서 받을 수 있고, 외부로 보낼 수 있다.
-    handleSubmit(event) {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        this.props.onSubmit(this.state.searchKeyword);
+        const searchKeyword = value;
+        onSubmit(searchKeyword);
     }
 
-    handleReset() {
-        this.props.onReset();
+    const handleReset = () => {
+        onReset();
     }
 
-    handleChangeInput(event) {
-        const searchKeyword = event.target.value;
-
-        if (searchKeyword.length <= 0) {
-            this.handleReset();
-        }
-
-        // 리액트에서 제공하는 메소드,  상태값을 변경하고 forceUpdate를 하지 않아도 데이터와 뷰가 변경된다.
-        // setState는 비동기 방식으로 변경된다.¡
-        this.setState({
-            searchKeyword: searchKeyword
-        });
+    const handleChangeInput = (event) => {
+        onChange(event.target.value);
     }
 
-    render() {
-        return (
-            <form onSubmit={event => this.handleSubmit(event)}
-                  onReset={() => this.handleReset()}>
-                {/*중괄호를 이용하여 자바스크립트 변수를 넣을 수 있다.*/}
-                <input type="text"
-                       name="form-input" placeholder="검색어를 입력하세요" autoFocus
-                       value={this.state.searchKeyword}
-                       onChange={event => this.handleChangeInput(event)}
-                />
+    return (
+        <form
+            onSubmit={handleSubmit}
+            onReset={handleReset}>
+            <input type="text"
+                   name="form-input"
+                   value={value}
+                   placeholder="검색어를 입력하세요"
+                   onChange={value => handleChangeInput(value)}/>
 
-                {/* 조건부 렌더링 삼항 연산 방식*/}
-                {/*{this.state.searchKeyword.length > 0 ?*/}
-                {/*    <button type="reset" className="btn-reset"></button> :*/}
-                {/*    null}*/}
-
-                {/* && 연산을 사용하는 방식*/}
-                {this.state.searchKeyword.length > 0 && <button type="reset" className="btn-reset"></button>}
-            </form>
-        );
-    }
-
+            {value.length > 0 && <button type = "reset" className="btn-reset"></button> }
+        </form>
+    )
 }
+
+export default SearchForm
